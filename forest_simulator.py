0323f1abb76f59999da3a098295aa837444db7d4
Kadzik0 <106621606+Kadzik0@users.noreply.github.com>
2022-08-01 17:13:54 +0200

    added gui

--- forest_simulator.py
+++ forest_simulator.py
@@ -13,10 +13,16 @@ todo list:
 import turtle as td
 import random as rd
 import tkinter as tk
+import threading
 
-# Define screen
 scr_size_x=800
 scr_size_y=600
+
+# adjustable variables
+no_of_predators=2
+no_of_peacefuls=6
+
+# Define screen
 wn = td.Screen()
 wn.title("Forest simulator")
 wn.bgcolor('grey')
@@ -37,8 +43,6 @@ pen.goto(-398, 298)
 pen.penup()
 pen.goto(-150, 320)
 pen.write("SYMULATOR ŻYCIA W LESIE", align='center', font=('Courier', 24, 'normal'))
-
-
 
 # Animal parent class inheriting after td.Turtle
 class Animal(td.Turtle):
@@ -126,41 +130,84 @@ class Peaceful(Animal):
 
             elif animal.type=='Predator':
                 pass
-    
+
     # method for breeding new peaceful animals
     def breed(self):
         list_of_animals.append(Peaceful())
         print('breed of turtle')
 
-# Test instance
-test = Predator(color='blue', shape='arrow', speed=1)
+def simulation():
+    # screenDefine()
+    window.destroy()
 
-# Create animal instances
-list_of_animals=[]
-for _ in range(rd.randint(5,9)):
-    list_of_animals.append(Peaceful())
-for _ in range(rd.randint(2,3)):
-    list_of_animals.append(Predator())
+    # Create animal instances
+    global list_of_animals
+    list_of_animals=[]
+    for _ in range(no_of_peacefuls):
+        list_of_animals.append(Peaceful())
+    for _ in range(no_of_predators):
+        list_of_animals.append(Predator())
+    # Run simulation
+    timer=0
+    while True:
+        wn.update()
 
-# print(list_of_animals)
+        for animal in list_of_animals:
+            animal.move()
+            for animal2 in list_of_animals:
+                if animal != animal2:
+                    animal.onCollision(animal2)
+            # test.onCollision(animal)
 
-# print(type(list_of_animals[0]))
-# print(type(Peaceful))
+        timer+=1
+
+class Gui:
+    def __init__(self, win):
+        button_start = tk.Button(win, text='Start symulacji', height=1, width=16, fg='black', bg='silver',
+                            command=simulation)
+        button_start.place(x=50, y=120)
+
+        self.label_no_predators = tk.Label(win, text=f'Number of predators: {no_of_predators}')
+        self.label_no_predators.place(x=100, y=50)
+        button_inc_no_predators = tk.Button(win, text='+', height=1, width=1, command=self.incrementPredators)
+        button_inc_no_predators.place(x=50, y=50)
+        button_dec_no_predators = tk.Button(win, text='-', height=1, width=1, command=self.decrementPredators)
+        button_dec_no_predators.place(x=30, y=50)
+
+        self.label_no_peacefuls = tk.Label(win, text=f'Number of peacefuls: {no_of_peacefuls}')
+        self.label_no_peacefuls.place(x=100, y=85)
+        button_inc_no_peacefuls = tk.Button(win, text='+', height=1, width=1, command=self.incrementPeacefuls)
+        button_inc_no_peacefuls.place(x=50, y=80)
+        button_dec_no_peacefuls = tk.Button(win, text='-', height=1, width=1, command=self.decrementPeacefuls)
+        button_dec_no_peacefuls.place(x=30, y=80)
+
+    def incrementPredators(self):
+        global no_of_predators
+        no_of_predators+=1
+        self.label_no_predators['text']=f'Number of predators: {no_of_predators}'
+
+    def decrementPredators(self):
+        global no_of_predators
+        if no_of_predators!=0:
+            no_of_predators-=1
+            self.label_no_peacefuls['text']=f'Number of predators: {no_of_predators}'
+
+    def incrementPeacefuls(self):
+        global no_of_peacefuls
+        no_of_peacefuls+=1
+        self.label_no_peacefuls['text']=f'Number of peacefuls: {no_of_peacefuls}'
+
+    def decrementPeacefuls(self):
+        global no_of_peacefuls
+        if no_of_peacefuls!=0:
+            no_of_peacefuls-=1
+            self.label_no_peacefuls['text']=f'Number of peacefuls: {no_of_peacefuls}'
 
 
-# Run simulation
-timer=0
-while True:
-    wn.update()
+# Define GUI
+window = tk.Tk()
+window.title('GUI')
+window.geometry('300x150')
+Gui(window)
 
-    for animal in list_of_animals:
-        animal.move()
-        for animal2 in list_of_animals:
-            if animal != animal2:
-                animal.onCollision(animal2)
-        # test.onCollision(animal)
-
-    test.move()
-
-    timer+=1
-    # print(timer)
+window.mainloop()
